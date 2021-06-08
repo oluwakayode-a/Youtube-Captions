7dee887541c0091f678d7cb052252ff195e73ace
oluwakayode-a <khaox07@gmail.com>
2021-06-08 14:08:32 +0100

    Fixed heroku port issue

--- app.py
+++ app.py
@@ -30,5 +30,6 @@ def get_text():
     return jsonify(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(debug=True, port=port)
 
